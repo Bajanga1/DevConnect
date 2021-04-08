ed19ca3b93c166619b01a68692538c264503f38b
steven miller <millersteven@live.com>
2021-04-08 15:07:21 -0400

    Finished experience and education components for dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,6 +5,8 @@ import Spinner from '../layout/Spinner'
 import { getCurrentProfile } from '../../actions/profile'
 import { Link } from 'react-router-dom'
 import DashboardActions from './DashboardActions'
+import Experience from './Experience'
+import Education from './Education'
 
 const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
     useEffect(() => {
@@ -20,6 +22,8 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
             {profile !== null ? (
                 <Fragment>
                     <DashboardActions />
+                    <Experience experience={profile.experience} />
+                    <Education education={profile.education} />
                 </Fragment> ) : (
                 <Fragment>
                     You have not yet setup a profile, please add some info
